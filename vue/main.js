e7dd8f9f91aaa88aafdaf62f5b436352187c4fe2
steffenpedersen <steffen.pedersen@live.dk>
2017-05-13 21:29:53 +0200

    🐣 first again

--- vue/main.js
+++ vue/main.js
@@ -1,20 +1,20 @@
 new Vue({
-  el: '#app',
-  data: {
-    posts: null,
-  },
-  methods: {
-    fetchData() {
-      const xhr = new XMLHttpRequest();
-      const self = this;
-      xhr.open('GET', 'data.json');
-      xhr.onload = () => {
-        self.posts = JSON.parse(xhr.responseText);
-      }
-      xhr.send();
-    },
-  },
-  created() {
-    this.fetchData();
-  },
+	el: '#app',
+	data: {
+		posts: null,
+	},
+	methods: {
+		fetchData() {
+			const xhr = new XMLHttpRequest();
+			const self = this;
+			xhr.open('GET', 'data.json');
+			xhr.onload = () => {
+				self.posts = JSON.parse(xhr.responseText);
+			}
+			xhr.send();
+		},
+	},
+	created() {
+		this.fetchData();
+	},
 });
